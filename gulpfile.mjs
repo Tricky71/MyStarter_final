08e1e3142a210b4a8dc475107c0486b387c2d1ce
unknown <nowwhat@mail.ru>
2026-07-02 15:18:57 +0400

    add img to webp & fixes

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,6 +1,8 @@
 import pkg from 'gulp';
 const { src, dest, watch, parallel, series } = pkg;
 
+import changed from 'gulp-changed';
+
 // import dartSass from 'sass';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
@@ -15,15 +17,71 @@ const bs = browserSync.create();
 
 import autoprefixer from 'gulp-autoprefixer';
 // import clean from 'gulp-clean';
-// import fonter from 'gulp-fonter';
-// import ttf2woff2 from 'gulp-ttf2woff2';
+import fonter from 'gulp-fonter';
+import ttf2woff2 from 'gulp-ttf2woff2';
 import notify from 'gulp-notify';
 
-import del from 'del';
+// import del from 'del';
+
+import { deleteAsync } from 'del'
+
+import webp from 'gulp-webp';
+
+import imagemin, { mozjpeg, optipng } from 'gulp-imagemin';
+
+// Пути к файлам
+const paths = {
+    src: 'app/img/src/**/*.{jpg,jpeg,png}', // Исходные картинки
+    dest: 'app/img/'                   // Папка назначения
+};
+
+//Обработка изображений
+// 1. Таск для сжатия оригиналов (сверяет JPG/PNG с папкой назначения)
+export const compressPngJpg = () => {
+    return src(paths.src, { base: 'app/img/src', encoding: false })
+        .pipe(changed(paths.dest)) // Пропускает уже сжатые JPG/PNG
+        .pipe(imagemin([
+            mozjpeg({ quality: 80, progressive: true }),
+            optipng({ optimizationLevel: 5 })
+        ]))
+        .pipe(dest(paths.dest))
+        .pipe(bs.stream());
+};
+
+// 2. Таск для создания WebP (сверяет исходники с расширением .webp в папке назначения)
+export const convertToWebp = () => {
+    return src(paths.src, { base: 'app/img/src', encoding: false })
+        .pipe(changed(paths.dest, { extension: '.webp' })) // Пропускает уже созданные .webp
+        .pipe(webp({ quality: 75 }))
+        .pipe(dest(paths.dest))
+        .pipe(bs.stream());
+};
+
+// 3. Объединяем их в один общий таск для удобства
+export const processImages = parallel(compressPngJpg, convertToWebp);
+
+// export const processImages = () => {
+//     return src(paths.src, { base: 'app/img/src', encoding: false })
+//         .pipe(changed(paths.dest)) // Пропускает уже сжатые JPG/PNG
+//         // 1. Сжимаем исходные JPG/PNG перед конвертацией
+//         .pipe(imagemin([
+//             mozjpeg({ quality: 80, progressive: true }),
+//             optipng({ optimizationLevel: 5 })
+//         ]))
+//         // 2. Сохраняем сжатые оригиналы (JPG/PNG) в папку назначения
+//         .pipe(dest(paths.dest))
+//         .pipe(bs.stream())
+//         // 3. Конвертируем в WebP со сжатием качества
+//         .pipe(webp({
+//             quality: 75 // Коэффициент качества WebP (0-100)
+//         }))
+//         // 4. Сохраняем готовые файлы
+//         .pipe(dest(paths.dest));
+// };
 
 export async function styles() {
   return src('app/scss/**/*.scss')
-    .pipe(scss({ style: 'compressed' }).on("error", notify.onError()))
+    .pipe(scss({ style: 'compressed', sourceMap: false  }).on("error", notify.onError()))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({ overrideBrowserslist: ['last 15 versions'] }))
     .pipe(dest('app/css'))
@@ -50,16 +108,11 @@ export function scripts() {
 //     .pipe(dest('app/fonts'));
 // }
 
-// export function fonts() {
-//   return src('app/fonts/src/**/*')
-//     // .pipe(fonter({
-//     //   formats: ['woff', 'ttf']
-//     // }))
-//     // .pipe(dest('app/fonts')) // промежуточная папка
-//     // .pipe(src('app/fonts/*.ttf'))
-//     .pipe(ttf2woff2())
-//     .pipe(dest('app/fonts'));
-// }
+export function fonts() {
+  return src('app/fonts/src/**/*.ttf') // Берем исходные TTF
+    .pipe(ttf2woff2())                 // Переводим в самый современный WOFF2
+    .pipe(dest('app/fonts'));          // Складываем готовый результат
+}
 
 export function watching() {
   bs.init({
@@ -70,7 +123,8 @@ export function watching() {
   });
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/main.js'], scripts);
-  // watch(['app/fonts/src'], fonts);
+  watch(['app/fonts/src'], fonts);
+  watch(['app/img/src/**/*.{jpg,jpeg,png}'], processImages);
   watch(['app/*.html']).on('change', bs.reload);
 }
 
@@ -82,7 +136,7 @@ export function watching() {
 // }
 
 export function delDist() {
-  return del(['dist/**/*',       // Выбираем всё содержимое dist
+  return deleteAsync(['dist/**/*',       // Выбираем всё содержимое dist
               '!dist/.git',      // Исключаем саму папку .git
               '!dist/.git/**']);
     // .pipe(clean());
@@ -102,5 +156,6 @@ export function building() {
 }
 
 // Экспорт задач
+// export default gulp.series(processImages);
 export const build = series(delDist, parallel(styles, scripts), building);
 export default parallel(styles, scripts, watching);
